a0c1cfbf6da338742781889e6ce260556e1e2fed
mithilprabhu2003 <mithilprabhu2003@gmail.com>
2023-07-11 13:23:52 +0530

    done

---
+++ src/Components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <h1>Home Page</h1>
+    <p>A change of latitude would help my attitude.
+    Travel is the movement of people between distant geographical locations. Travel can be done by foot, bicycle, automobile, 
+    train, boat, bus, airplane, ship or other means, with or without luggage, and can be one way or round trip.
+    The primary role of a travel agent is to help people make travel arrangements, which might include booking flights, hotels, sightseeing tours, and making dining recommendations.
+     A travel agent assesses each customer's unique needs, preferences, and budget to ensure their trip goes as smoothly as possible.
+    </p>
+    </>
+  )
+}
+
+export default Home
